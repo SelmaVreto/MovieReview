dc50f4913a1b17910eee95015cde3f335b0a645a
SelmaVreto <selma.vreto@stu.ibu.edu.ba>
2022-03-20 18:58:44 +0100

    dbtest

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
 <?php
 echo "Movie";
+
+$servername = "localhost";
+$username = "moviesrandr";
+$password = "movie";
+$schema = "moviesr"
+
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=$schema", $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo "Connected successfully";
+} catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
+}
+
  ?>
