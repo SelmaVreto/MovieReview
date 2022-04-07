9208e5b1e4c22cbcc0928614895d51e1966ce5e2
SelmaVreto <selma.vreto@stu.ibu.edu.ba>
2022-04-08 01:12:47 +0200

    Flight base

--- rest/index.php
+++ rest/index.php
@@ -3,6 +3,25 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-require_once dirname(__FILE__)."/dao/baseDao.class.php";
+require dirname(__FILE__).'/../vendor/autoload.php';
 
-$dao = new baseDao();
+Flight::route('/', function(){
+  echo "Hello";
+});
+
+Flight::route('/genre', function(){
+  $dao = new genreDao();
+});
+Flight::route('/3', function(){
+  echo "Hello Selma";
+});
+Flight::route('/4', function(){
+  echo "Hello Selma";
+});
+Flight::route('/5', function(){
+  echo "Hello Selma";
+});
+
+Flight::start();
+
+?>
