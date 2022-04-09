9d23300fa584428e9614927870d1e3698f7597dd
SelmaVreto <selma.vreto@stu.ibu.edu.ba>
2022-04-10 00:40:17 +0200

    paring

--- rest/index.php
+++ rest/index.php
@@ -3,42 +3,27 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-require dirname(__FILE__).'/../vendor/autoload.php';
-require dirname(__FILE__).'/dao/movieDao.class.php';
-require dirname(__FILE__).'/dao/genreDao.class.php';
+require_once dirname(__FILE__).'/../vendor/autoload.php';
+require_once dirname(__FILE__).'/dao/movieDao.class.php';
+require_once dirname(__FILE__).'/dao/genreDao.class.php';
+
+/* utility function for reading query parameters from URL */
+Flight::map('query', function($name, $default_value = NULL){
+  $request = Flight::request();
+  $query_param = @$request->query->getData()[$name];
+  $query_param = $query_param ? $query_param : $default_value;
+  return $query_param;
+});
 
 Flight::register('genreDao', 'genreDao');
+require_once dirname(__FILE__).'/routes/genre.php';
+
 Flight::register('movieDao', 'movieDao');
 
 Flight::route('/', function(){
   echo "Hello";
 });
 
-Flight::route('GET /genre', function(){
-  Flight::json(Flight::genreDao()->get_all(0,10));
-});
-
-Flight::route('GET /genre/@id', function($id){
-  Flight::json(Flight::genreDao()->get_by_id($id));
-
-
-});
-Flight::route('POST /genre', function(){
-  $request = Flight::request();
-  $data = $request->data->getData();
-  $dao = new genreDao();
-  $genre = $dao->add($data);
-  Flight::json($genre);
-});
-
-Flight::route('PUT /genre/@id', function($id){
-  $dao = new genreDao();
-  $request = Flight::request();
-  $data = $request->data->getData();
-  $dao->update($id,$data);
-  $genre=$dao->get_by_id($id);
-    Flight::json($genre);
-});
 
 Flight::route('GET /movie', function(){
   Flight::json(Flight::movieDao()->get_all(0,10));
---
+++ rest/routes/genre.php
@@ -0,0 +1,32 @@
+<?php
+
+Flight::route('GET /genre', function(){
+  $offset = Flight::query('offset', 0);
+  $limit = Flight::query('limit', 10);
+
+  Flight::json(Flight::genreDao()->get_all($offset,$limit));
+});
+
+Flight::route('GET /genre/@id', function($id){
+  Flight::json(Flight::genreDao()->get_by_id($id));
+
+
+});
+Flight::route('POST /genre', function(){
+  $request = Flight::request();
+  $data = $request->data->getData();
+  $dao = new genreDao();
+  $genre = $dao->add($data);
+  Flight::json($genre);
+});
+
+Flight::route('PUT /genre/@id', function($id){
+  $dao = new genreDao();
+  $request = Flight::request();
+  $data = $request->data->getData();
+  $dao->update($id,$data);
+  $genre=$dao->get_by_id($id);
+    Flight::json($genre);
+});
+
+ ?>
